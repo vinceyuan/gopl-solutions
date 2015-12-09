6f414c5cdbd3712adbe870457a5d905da0d5d32a
Vince Yuan <vince.yuan@gmail.com>
2015-12-10 02:30:13 +0800

    Added ex2.1

---
+++ ch02/ex2.1/ex2.1.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/vinceyuan/gopl-solutions/ch02/ex2.1/tempconv"
+)
+
+func main() {
+	fmt.Println(tempconv.CToK(tempconv.FreezingC))
+}
---
+++ ch02/ex2.1/tempconv/tempconv.go
@@ -0,0 +1,28 @@
+package tempconv
+
+import "fmt"
+
+type Celsius float64
+type Fahrenheit float64
+type Kelvin float64
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC     Celsius = 0
+	BoilingC      Celsius = 100
+	ZeroK         Kelvin  = -273.15
+)
+
+func (c Celsius) String() string    { return fmt.Sprintf("%g°C", c) }
+func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
+func (k Kelvin) String() string     { return fmt.Sprintf("%g°K", k) }
+
+// CToF converts a Celsius temperature to Fahrenheit.
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+
+// FToC converts a Fahrenheit temperature to Celsius.
+func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+
+func CToK(c Celsius) Kelvin { return Kelvin(1000.0 * (c - AbsoluteZeroC)) }
+
+func KToC(k Kelvin) Celsius { return Celsius(k/1000.0 - Kelvin(float64(AbsoluteZeroC))) }
