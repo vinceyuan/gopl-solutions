f1c941630ef8d6dd328931be075614612ea3a17d
Vince Yuan <vince.yuan@gmail.com>
2015-12-21 19:34:40 +0800

    Added ex4.3

---
+++ ch04/ex4.3/ex4.3.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	//!+array
+	a := [...]int{0, 1, 2, 3, 4, 5}
+	reverse(&a)
+	fmt.Println(a) // "[5 4 3 2 1 0]"
+	//!-array
+}
+
+// reverse reverses an array of ints in place.
+func reverse(s *[6]int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
