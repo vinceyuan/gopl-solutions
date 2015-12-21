ecbe92790db678c20b94a25ecb5295534c2dcc7f
Vince Yuan <vince.yuan@gmail.com>
2015-12-21 11:45:03 +0800

    Added ex3.13

---
+++ ch03/ex3.13/ex3.13.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+const (
+	KB = 1000
+	MB = KB * KB
+	GB = MB * KB
+	TB = GB * KB
+	PB = TB * KB
+	EB = PB * KB
+	ZB = EB * KB
+	YB = ZB * KB
+)
+
+func main() {
+	fmt.Println(KB, MB, GB, TB, PB, EB)
+}
