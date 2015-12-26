eb3b40ad4110586288912ded5a509830ec0880da
Vince Yuan <vince.yuan@gmail.com>
2015-12-27 00:28:19 +0800

    Added ex5.7

---
+++ ch05/ex5.7/ex5.7.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		outline(url)
+	}
+}
+
+func outline(url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	forEachNode(doc, startElement, endElement)
+	return nil
+}
+
+// forEachNode calls the functions pre(x) and post(x) for each node
+// x in the tree rooted at n. Both functions are optional.
+// pre is called before the children are visited (preorder) and
+// post is called after (postorder).
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+
+	if post != nil {
+		post(n)
+	}
+}
+
+var depth int
+
+func startElement(n *html.Node) {
+	if n.Type == html.ElementNode {
+		fmt.Printf("%*s<%s", depth*2, "", n.Data)
+		for _, a := range n.Attr {
+			fmt.Printf(" %s=\"%s\"", a.Key, a.Val)
+		}
+		if n.FirstChild != nil {
+			fmt.Printf(">\n")
+		} else {
+			fmt.Printf(" />\n")
+		}
+		depth++
+	} else if n.Type == html.TextNode {
+		trimmed := strings.TrimSpace(n.Data)
+		if trimmed != "" {
+			fmt.Printf("%*s%s\n", depth*2, "", trimmed)
+		}
+	} else if n.Type == html.CommentNode {
+		fmt.Printf("%*s<!--%s-->\n", depth*2, "", n.Data)
+	}
+}
+
+func endElement(n *html.Node) {
+	if n.Type == html.ElementNode {
+		depth--
+		if n.FirstChild != nil {
+			fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
+		}
+	}
+}
