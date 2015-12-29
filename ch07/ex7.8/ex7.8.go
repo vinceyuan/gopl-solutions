28c903178e818db25699d7f4e5277940214db283
Vince Yuan <vince.yuan@gmail.com>
2015-12-29 19:31:22 +0800

    Added ex7.8

---
+++ ch07/ex7.8/ex7.8.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+	"text/tabwriter"
+	"time"
+)
+
+type Track struct {
+	Title  string
+	Artist string
+	Album  string
+	Year   int
+	Length time.Duration
+}
+
+var tracks = []*Track{
+	{"Go", "Delilah", "From the Roots Up", 2012, length("3m38s")},
+	{"Go", "Moby", "Moby", 1992, length("3m37s")},
+	{"Go Ahead", "Alicia Keys", "As I Am", 2007, length("4m36s")},
+	{"Ready 2 Go", "Martin Solveig", "Smash", 2011, length("4m24s")},
+}
+
+func length(s string) time.Duration {
+	d, err := time.ParseDuration(s)
+	if err != nil {
+		panic(s)
+	}
+	return d
+}
+
+func printTracks(tracks []*Track) {
+	const format = "%v\t%v\t%v\t%v\t%v\t\n"
+	tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
+	fmt.Fprintf(tw, format, "Title", "Artist", "Album", "Year", "Length")
+	fmt.Fprintf(tw, format, "-----", "------", "-----", "----", "------")
+	for _, t := range tracks {
+		fmt.Fprintf(tw, format, t.Title, t.Artist, t.Album, t.Year, t.Length)
+	}
+	tw.Flush() // calculate column widths and print table
+}
+
+type byArtist []*Track
+
+func (x byArtist) Len() int           { return len(x) }
+func (x byArtist) Less(i, j int) bool { return x[i].Artist < x[j].Artist }
+func (x byArtist) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
+
+type byYear []*Track
+
+func (x byYear) Len() int           { return len(x) }
+func (x byYear) Less(i, j int) bool { return x[i].Year < x[j].Year }
+func (x byYear) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
+
+type byTitleAndArtist []*Track
+
+func (x byTitleAndArtist) Len() int { return len(x) }
+func (x byTitleAndArtist) Less(i, j int) bool {
+	if x[i].Title == x[j].Title {
+		return x[i].Artist < x[j].Artist
+	} else {
+		return x[i].Title < x[j].Title
+	}
+}
+func (x byTitleAndArtist) Swap(i, j int) { x[i], x[j] = x[j], x[i] }
+
+func main() {
+	fmt.Println("\nbyTitleAndArtist:")
+	sort.Sort(byTitleAndArtist(tracks))
+	printTracks(tracks)
+}
+
+/*
+//!+artistoutput
+Title       Artist          Album              Year  Length
+-----       ------          -----              ----  ------
+Go Ahead    Alicia Keys     As I Am            2007  4m36s
+Go          Delilah         From the Roots Up  2012  3m38s
+Ready 2 Go  Martin Solveig  Smash              2011  4m24s
+Go          Moby            Moby               1992  3m37s
+//!-artistoutput
+
+//!+artistrevoutput
+Title       Artist          Album              Year  Length
+-----       ------          -----              ----  ------
+Go          Moby            Moby               1992  3m37s
+Ready 2 Go  Martin Solveig  Smash              2011  4m24s
+Go          Delilah         From the Roots Up  2012  3m38s
+Go Ahead    Alicia Keys     As I Am            2007  4m36s
+//!-artistrevoutput
+
+//!+yearoutput
+Title       Artist          Album              Year  Length
+-----       ------          -----              ----  ------
+Go          Moby            Moby               1992  3m37s
+Go Ahead    Alicia Keys     As I Am            2007  4m36s
+Ready 2 Go  Martin Solveig  Smash              2011  4m24s
+Go          Delilah         From the Roots Up  2012  3m38s
+//!-yearoutput
+
+//!+customout
+Title       Artist          Album              Year  Length
+-----       ------          -----              ----  ------
+Go          Moby            Moby               1992  3m37s
+Go          Delilah         From the Roots Up  2012  3m38s
+Go Ahead    Alicia Keys     As I Am            2007  4m36s
+Ready 2 Go  Martin Solveig  Smash              2011  4m24s
+//!-customout
+*/
+
+type customSort struct {
+	t    []*Track
+	less func(x, y *Track) bool
+}
+
+func (x customSort) Len() int           { return len(x.t) }
+func (x customSort) Less(i, j int) bool { return x.less(x.t[i], x.t[j]) }
+func (x customSort) Swap(i, j int)      { x.t[i], x.t[j] = x.t[j], x.t[i] }
