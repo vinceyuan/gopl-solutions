09b3f195378e8a55b30dc1cf301380ead17abb2f
Vince Yuan <vince.yuan@gmail.com>
2015-12-31 15:56:42 +0800

    Added ex8.3

---
+++ ch08/ex8.3/ex8.3.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	done := make(chan struct{})
+	go func() {
+		io.Copy(os.Stdout, conn) // NOTE: ignoring errors
+		log.Println("done")
+		done <- struct{}{} // signal the main goroutine
+	}()
+	mustCopy(conn, os.Stdin)
+	if tcpconn, ok := conn.(*net.TCPConn); ok {
+		tcpconn.CloseWrite()
+	}
+	<-done // wait for background goroutine to finish
+}
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
+}
