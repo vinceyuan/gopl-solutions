44f22ba2e4e0fad799e0078e395c267505a2050e
yurianxdev <42949739+yurianxdev@users.noreply.github.com>
2020-07-24 12:23:13 -0500

    Tweak implementation on exercise 3.10 with bytes.Buffer (#11)
    * Implement exercise 3.10 with bytes.Buffer
    
    * Improvements in comments

--- ch03/ex3.10/ex3.10.go
+++ ch03/ex3.10/ex3.10.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 )
@@ -11,17 +12,24 @@ func main() {
 	}
 }
 
-// comma inserts commas in a non-negative decimal integer string.
+// Comma inserts commas in a non-negative decimal integer string.
 func comma(s string) string {
-	n := len(s)
-	i := n % 3
-	if i == 0 {
-		i += 3
+	// Set the buffer with it 0 value
+	var buff bytes.Buffer
+	// Number of digits before needing a comma
+	remainder := len(s) % 3
+
+	for i, v := range s {
+		// Write just the value if isn't a multiple of 3 after the reminder
+		if i < remainder || !((i-remainder)%3 == 0 && i != 0) {
+			_, _ = fmt.Fprintf(&buff, "%c", v)
+			continue
+		}
+
+		// Write a comma each 3 digits, then just write the rune
+		buff.WriteByte(',')
+		_, _ = fmt.Fprintf(&buff, "%c", v)
 	}
-	for i < n {
-		s = s[:i] + "," + s[i:]
-		i += 4
-		n++
-	}
-	return s
+
+	return buff.String()
 }
